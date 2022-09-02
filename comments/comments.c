3104a1fe34fd90d4470b182919775db042bd8d84
Gustavo Ferreira <gustavodelferreira.12@gmail.com>
2022-09-02 09:44:02 -0300

    comments

---
+++ comments/comments.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  /* 
+    printf("Comments are boring");
+  */
+
+  // This prints out text!
+  printf("Comments are fun");
+
+  return 0;
+}
