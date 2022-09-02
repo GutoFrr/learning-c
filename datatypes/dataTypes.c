428a40aa91892addd1efbda031acad6d3148ad16
Gustavo Ferreira <gustavodelferreira.12@gmail.com>
2022-09-02 09:19:54 -0300

    data types: number, decimals, characters, string

---
+++ datatypes/dataTypes.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int age = 40;                       // number
+  double gpa = 3.7;                   // decimal number
+  char grade = 'A';                   // single character
+  char phrase[] = "Gustavo Ferreira"; // string of characters
+
+  return 0;
+}
