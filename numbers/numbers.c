9c458fe414398d32b80eafd1ffe307e7245abb69
Gustavo Ferreira <gustavodelferreira.12@gmail.com>
2022-09-02 09:38:01 -0300

    working with numbers: operations, pow, ceil, floor

---
+++ numbers/numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int num = 6;
+
+  printf("%d\n", num);
+  printf("%f\n", pow(2, 3));
+  printf("%f\n", ceil(36.631));
+  printf("%f\n", floor(36.631));
+  return 0;
+}
